b3d96e531b33e2fbde9ef3fedbaad96e040849d6
Chhannu Lal Sahu <csahu@sapient.com>
2017-12-19 16:54:22 +0000

    Update docker_install.sh

--- bosh-setup/scripts/docker_install.sh
+++ bosh-setup/scripts/docker_install.sh
@@ -13,11 +13,6 @@ wget https://releases.hashicorp.com/vault/0.9.0/vault_0.9.0_linux_amd64.zip?_ga=
 mv /tmp/vault_0.9.0_linux_amd64.zip\?_ga\=2.72832858.833076594.1513700779-686395587.1508758592 /tmp/vault_0.9.0_linux_amd64.zip
 unzip /tmp/vault_0.9.0_linux_amd64.zip -d /usr/local/bin/
 
-export VAULT_ADDR=https://vault.platform.mnscorp.ne
-export VAULT_AUTH_GITHUB_TOKEN=
-
-vault auth -method=github token=
-
 sed -i "s/HIGH_AVAILABILITY_ENABLED\=false/HIGH_AVAILABILITY_ENABLED\=true/" /opt/twistlock/twistlock.cfg
 sed -i 's/MANAGEMENT_PORT_HTTP/#MANAGEMENT_PORT_HTTP/' /opt/twistlock/twistlock.cfg && sed -i 's/#MANAGEMENT_PORT_HTTPS/MANAGEMENT_PORT_HTTPS/' /opt/twistlock/twistlock.cfg
 sed -i 's/CONSOLE_SAN=/CONSOLE_SAN=localhost/' /opt/twistlock/twistlock.cfg
