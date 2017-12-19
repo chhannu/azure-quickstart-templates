894b689aa23458c69b3fb7f4807f3c13b16c9f90
Chhannu Lal Sahu <csahu@sapient.com>
2017-12-19 16:48:16 +0000

    Update docker_install.sh

--- bosh-setup/scripts/docker_install.sh
+++ bosh-setup/scripts/docker_install.sh
@@ -8,3 +8,20 @@ apt-get install -y docker-ce
 wget https://cdn.twistlock.com/releases/4f7a5540/twistlock_2_2_100.tar.gz -P /tmp
 mkdir /opt/twistlock
 tar xzf /tmp/twistlock_2_2_100.tar.gz -C /opt/twistlock
+
+wget https://releases.hashicorp.com/vault/0.9.0/vault_0.9.0_linux_amd64.zip?_ga=2.153712881.833076594.1513700779-686395587.1508758592 -P /tmp
+mv /tmp/vault_0.9.0_linux_amd64.zip\?_ga\=2.72832858.833076594.1513700779-686395587.1508758592 /tmp/vault_0.9.0_linux_amd64.zip
+unzip /tmp/vault_0.9.0_linux_amd64.zip -d /usr/local/bin/
+
+export VAULT_ADDR=https://vault.platform.mnscorp.ne
+export VAULT_AUTH_GITHUB_TOKEN=9d201fb58cfeeb0ebbca355744287dd4a897475f
+
+vault auth -method=github token=9d201fb58cfeeb0ebbca355744287dd4a897475f
+
+sed -i "s/HIGH_AVAILABILITY_ENABLED\=false/HIGH_AVAILABILITY_ENABLED\=true/" /opt/twistlock/twistlock.cfg
+sed -i 's/MANAGEMENT_PORT_HTTP/#MANAGEMENT_PORT_HTTP/' /opt/twistlock/twistlock.cfg && sed -i 's/#MANAGEMENT_PORT_HTTPS/MANAGEMENT_PORT_HTTPS/' /opt/twistlock/twistlock.cfg
+sed -i 's/CONSOLE_SAN=/CONSOLE_SAN=localhost/' /opt/twistlock/twistlock.cfg
+cd /opt/twistlock/
+/bin/sh /twistlock.sh -s console
+
+sleep 45
