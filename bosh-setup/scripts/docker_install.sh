e36284f73fb57854e2ae1254b2e45e474a2c0a3d
Chhannu Lal Sahu <csahu@sapient.com>
2017-12-21 12:46:50 +0000

    Update docker_install.sh

--- bosh-setup/scripts/docker_install.sh
+++ bosh-setup/scripts/docker_install.sh
@@ -4,11 +4,3 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(
 apt-get update
 apt-cache policy docker-ce
 apt-get install -y docker-ce
-
-wget https://cdn.twistlock.com/releases/4f7a5540/twistlock_2_2_100.tar.gz -P /tmp
-mkdir /opt/twistlock
-tar xzf /tmp/twistlock_2_2_100.tar.gz -C /opt/twistlock
-
-wget https://releases.hashicorp.com/vault/0.9.0/vault_0.9.0_linux_amd64.zip?_ga=2.153712881.833076594.1513700779-686395587.1508758592 -P /tmp
-mv /tmp/vault_0.9.0_linux_amd64.zip\?_ga\=2.72832858.833076594.1513700779-686395587.1508758592 /tmp/vault_0.9.0_linux_amd64.zip
-unzip /tmp/vault_0.9.0_linux_amd64.zip -d /usr/local/bin/
