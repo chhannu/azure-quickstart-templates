32389343cc6559d1549d274302fdb4da1ab7663a
Chhannu Lal Sahu <csahu@sapient.com>
2017-12-21 12:22:15 +0000

    Update docker_install.sh

--- bosh-setup/scripts/docker_install.sh
+++ bosh-setup/scripts/docker_install.sh
@@ -1,4 +1,7 @@
 apt-get -y install nginx curl wget unzip
+apt-get -y install python-pip
+pip install ansible
+pip install ansible-modules-hashivault
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
