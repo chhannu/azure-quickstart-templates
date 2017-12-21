26d0194a47338a1b08060ffc5ac29a6e66705249
Chhannu Lal Sahu <csahu@sapient.com>
2017-12-21 12:32:54 +0000

    Update docker_install.sh

--- bosh-setup/scripts/docker_install.sh
+++ bosh-setup/scripts/docker_install.sh
@@ -1,7 +1,4 @@
 apt-get -y install nginx curl wget unzip
-apt-get -y install python-pip
-pip install ansible
-pip install ansible-modules-hashivault
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
@@ -15,11 +12,3 @@ tar xzf /tmp/twistlock_2_2_100.tar.gz -C /opt/twistlock
 wget https://releases.hashicorp.com/vault/0.9.0/vault_0.9.0_linux_amd64.zip?_ga=2.153712881.833076594.1513700779-686395587.1508758592 -P /tmp
 mv /tmp/vault_0.9.0_linux_amd64.zip\?_ga\=2.72832858.833076594.1513700779-686395587.1508758592 /tmp/vault_0.9.0_linux_amd64.zip
 unzip /tmp/vault_0.9.0_linux_amd64.zip -d /usr/local/bin/
-
-#sed -i "s/HIGH_AVAILABILITY_ENABLED\=false/HIGH_AVAILABILITY_ENABLED\=true/" /opt/twistlock/twistlock.cfg
-#sed -i 's/MANAGEMENT_PORT_HTTP/#MANAGEMENT_PORT_HTTP/' /opt/twistlock/twistlock.cfg && sed -i 's/#MANAGEMENT_PORT_HTTPS/MANAGEMENT_PORT_HTTPS/' /opt/twistlock/twistlock.cfg
-#sed -i 's/CONSOLE_SAN=/CONSOLE_SAN=localhost/' /opt/twistlock/twistlock.cfg
-#cd /opt/twistlock/
-#/bin/sh /twistlock.sh -s console
-
-#sleep 45
