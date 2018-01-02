40832f21b17e22a432b645608c4099b4627a0d53
Chhannu Lal Sahu <csahu@sapient.com>
2018-01-02 12:42:58 +0000

    Update only_docker.sh

--- bosh-setup/scripts/only_docker.sh
+++ bosh-setup/scripts/only_docker.sh
@@ -1,5 +1,9 @@
+apt-get -y install nginx curl wget unzip
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-cache policy docker-ce
 apt-get install -y docker-ce
+apt-get install -y python-pip
+pip install ansible
+pip install ansible-modules-hashivault
