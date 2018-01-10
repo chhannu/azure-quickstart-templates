5aa20667172e40573730ae9cb0c6934550a26740
Chhannu Lal Sahu <csahu@sapient.com>
2018-01-10 12:27:24 +0000

    Update only_docker.sh

--- bosh-setup/scripts/only_docker.sh
+++ bosh-setup/scripts/only_docker.sh
@@ -7,3 +7,7 @@ apt-get install -y docker-ce
 apt-get install -y python-pip
 pip install ansible
 pip install ansible-modules-hashivault
+wget https://cdn.twistlock.com/releases/4f7a5540/twistlock_2_2_100.tar.gz -P /tmp
+mkdir /opt/twistlock
+tar xzf /tmp/twistlock_2_2_100.tar.gz -C /opt/twistlock
+echo "$1" > /opt/check
