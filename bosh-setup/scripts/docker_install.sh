5722f18eabdaec0dd51e784451536ebbb174c06d
Chhannu Lal Sahu <csahu@sapient.com>
2018-01-15 14:53:55 +0000

    Update docker_install.sh

--- bosh-setup/scripts/docker_install.sh
+++ bosh-setup/scripts/docker_install.sh
@@ -1,3 +1,12 @@
+apt-get -y install nginx curl wget unzip
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-cache policy docker-ce
+apt-get install -y docker-ce
+apt-get install -y python-pip
+pip install ansible
+pip install ansible-modules-hashivault
 wget https://cdn.twistlock.com/releases/4f7a5540/twistlock_2_2_100.tar.gz -P /tmp
 mkdir /opt/twistlock
 tar xzf /tmp/twistlock_2_2_100.tar.gz -C /opt/twistlock
